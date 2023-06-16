37837bb136352bd3776c5e6f5122395e1f5306a7
Diego <Diego@DESKTOP-F7B7O60>
2023-06-15 21:01:00 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Copia el path al que deseas navegar: ");
+string path = Console.ReadLine();
+string[] files = Directory.GetFiles(path);
+string[] folders = Directory.GetDirectories(path);
+
+Console.WriteLine("Estos son los archivos: \n");
+foreach (string file in files)
+{
+    string filter = file.Replace(path, "-");
+    Console.WriteLine($"\t {filter}");
+}
+
+
+Console.WriteLine("Estas son las carpetas: \n");
+foreach (string folder in folders)
+{
+    string filter = folder.Replace(path, "-");
+    Console.WriteLine($"\t {filter}");
+}
+
+Console.ReadKey();
